9a66722e97e1d869c37fd2de228f140353b8a02f
matheusvfg <matheusvfg87@gmail.com>
2025-05-06 12:10:32 -0300

    criação da pagina inicial

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { IonPage } from '@ionic/react';
+import ExploreContainer from '../../components/ExploreContainer';
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <ExploreContainer />
+    </IonPage>
+  );
+};
+
+export default Home;
